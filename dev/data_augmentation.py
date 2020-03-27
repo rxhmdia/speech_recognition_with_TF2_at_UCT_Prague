e019c1e3911c604b9b0327399dacd6c0c8bf47d2
Martin Vejvar <vejvarm@gmail.com>
2020-03-27 12:04:20 +0100

    feat: finish example of Time and Frequency masking SpecAug

--- dev/data_augmentation.py
+++ dev/data_augmentation.py
@@ -4,13 +4,14 @@ import numpy as np
 import tensorflow as tf
 from helpers import console_logger
 
-_NUM_SAMPLES = 20
+_NUM_SAMPLES = 2
 _DATA_SHAPE = (10, 5)  # time, num_features
 _BATCH_SIZE = 1
 _NUM_INSTANCES = 1  # > 1 not supported
 _BANDWIDTH = 2
+_AXIS = 1
 
-LOGGER = console_logger(__name__, "DEBUG")
+LOGGER = console_logger('tensorflow', "DEBUG")
 
 # TODO:
 #  - TimeMasking
@@ -43,37 +44,47 @@ class ArtificialDataset(tf.data.Dataset):
 #  _freqaug will be second sublass
 
 
-def _mask_sample(sample, num_instances=_NUM_INSTANCES, bandwidth=_BANDWIDTH):
+def _mask_sample(sample, axis=_AXIS, num_instances=_NUM_INSTANCES, bandwidth=_BANDWIDTH):
     # tm_lb = np.random.randint(0, sample.shape[0]-bandwidth)
     # tm_ub = tm_lb + bandwidth
 
+    if axis == 1:
+        sample = tf.transpose(sample, (1, 0))
+
     for i in range(num_instances):
-        ntime, nfreq = sample.shape
-        tm_lb = tf.random.uniform([], 0, ntime-bandwidth, dtype=tf.int32)  # lower bounds
+        nrows, _ = sample.shape
+        tm_lb = tf.random.uniform([], 0, nrows-bandwidth, dtype=tf.int32)  # lower bounds
         tm_ub = tm_lb + bandwidth  # upper bounds
         # LOGGER.info(f"tm_lb: {tm_lb}, tm_ub: {tm_ub}")
 
         mask = tf.concat((tf.ones((tm_lb, ), dtype=tf.bool),
                          tf.zeros((bandwidth, ), dtype=tf.bool),
-                         tf.ones((ntime-tm_ub, ), dtype=tf.bool)), axis=0)
+                         tf.ones((nrows-tm_ub, ), dtype=tf.bool)), axis=0)
         # LOGGER.debug(f"tm.shape: {mask.shape}")
 
         sample = tf.boolean_mask(sample, mask)
+
+    if axis == 1:
+        sample = tf.transpose(sample, (1, 0))
 
     return sample
 
 
 @tf.function
 def mask_time(x):
-    return tf.map_fn(_mask_sample, x, parallel_iterations=4)
+    return tf.map_fn(lambda inp: _mask_sample(inp, axis=0), x, parallel_iterations=4)
 
 
-def freqmasking():
-    pass
+@tf.function
+def mask_freq(x):
+    return tf.map_fn(lambda inp: _mask_sample(inp, axis=1), x, parallel_iterations=4)
 
 
 if __name__ == '__main__':
-    ds = ArtificialDataset().batch(_BATCH_SIZE).map(mask_time, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds = (ArtificialDataset()
+          .batch(_BATCH_SIZE)
+          .map(mask_time, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+          .map(mask_freq, num_parallel_calls=tf.data.experimental.AUTOTUNE))
 
     for sample in ds:
         LOGGER.debug(f"sample.shape: {sample.shape}")
